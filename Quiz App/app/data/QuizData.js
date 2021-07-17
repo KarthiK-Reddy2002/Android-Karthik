d68bead6e87fc0f39de8ebc74dfcafe18369428f
KarthiK-Reddy2002 <83204255+KarthiK-Reddy2002@users.noreply.github.com>
2021-07-17 21:59:52 +0530

    project

---
+++ Quiz App/app/data/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "India’s first satellite of 2020 GSAT-30 was successfully launched is a",
+        options: ["Remote Sensing Satellite","Communication Satelite","Spy Satellite","Metrological Satellite"],
+        correct_option: "Communication Satelite"
+    },
+    {
+        question: "Which country operationalized world’s largest radio telescope?",
+        options: ["USA","China","Russia","India"],
+        correct_option: "China"
+    },
+    {
+        question: "Which one among the following radiations carries maximum energy?",
+        options: ["Ultraviolet rays"," Gamma rays"," X- rays","Infra-red rays"],
+        correct_option: " Gamma rays"
+    },
+    {
+        question: "The head quarters of world trade organization is in",
+        options: ["Montreal","Seattle","Geneva","the Hague"],
+        correct_option: "Geneva"
+    },
+    {
+        question: " The instrument useful for measuring curvature of surface is",
+        options: ["Odometer","Spherometer"," Tachometer","Speedometer"],
+        correct_option: "Spherometer"
+    }
+]
